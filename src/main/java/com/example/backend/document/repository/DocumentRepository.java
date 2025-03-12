56e6914f919588b5b3d421fd9becdc1497b3c35a
chanmi12 <chanme1213@gmail.com>
2025-03-12 13:20:04 +0900

    요청한 문서 리스트 정보 관련 쿼리 수정

--- src/main/java/com/example/backend/document/repository/DocumentRepository.java
+++ src/main/java/com/example/backend/document/repository/DocumentRepository.java
@@ -16,19 +16,19 @@ public interface DocumentRepository extends JpaRepository<Document, Long> {
     @Query("SELECT d FROM Document d JOIN SignatureRequest s ON d.id = s.document.id WHERE s.signerEmail = :email")
     List<Document> findDocumentsBySignerEmail(@Param("email") String email);
 
-    @Query("SELECT d.id, d.fileName, d.createdAt, d.status, d.requestName, s.expiredAt " +
+    @Query("SELECT DISTINCT d.id, d.fileName, d.createdAt, d.status, d.requestName, s.expiredAt " +
             "FROM Document d " +
             "LEFT JOIN SignatureRequest s ON d.id = s.document.id " +
             "WHERE d.member.uniqueId = :uniqueId")
     List<Object[]> findDocumentsWithExpiration(@Param("uniqueId") String uniqueId);
 
-
-    @Query("SELECT d.id, d.fileName, d.createdAt, d.status, m.name, d.requestName, s.expiredAt " +
+    @Query("SELECT DISTINCT d.id, d.fileName, d.createdAt, d.status, m.name, d.requestName, s.expiredAt " +
             "FROM Document d " +
             "JOIN d.member m " +
             "JOIN SignatureRequest s ON d.id = s.document.id " +
             "WHERE s.signerEmail = :email")
     List<Object[]> findDocumentsBySignerEmailWithRequester(@Param("email") String email);
+
 
     @Modifying
     @Transactional
