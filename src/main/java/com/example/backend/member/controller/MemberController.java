dde07c411b638b4880d713db605ef5a65146aaa6
HongKing896 <butyking7896@gmail.com>
2025-04-08 21:36:53 +0900

    feat: 로그인 시 Jwt토큰 필터에서 로그인 정보 SecurityContextHolder에 저장

--- src/main/java/com/example/backend/member/controller/MemberController.java
+++ src/main/java/com/example/backend/member/controller/MemberController.java
@@ -1,10 +1,14 @@
 package com.example.backend.member.controller;
 
+import com.example.backend.auth.dto.AuthDto;
 import com.example.backend.member.DTO.SearchMemberDTO;
 import com.example.backend.member.service.MemberService;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/member")
 @RequiredArgsConstructor
@@ -43,5 +48,18 @@ public class MemberController {
             System.out.println(searchMemberDTO.getEmail());
         }
         return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<AuthDto> fetchLoginUserData() {
+        log.debug("fetching Logged in UserData...");
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        if (principal instanceof AuthDto) {
+            AuthDto authDto = (AuthDto) principal;
+            return ResponseEntity.ok(authDto);
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
     }
 }
